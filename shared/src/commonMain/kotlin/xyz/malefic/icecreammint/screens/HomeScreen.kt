3ea6896c714b9323caae48b7c906361add4c3966
Om Gupta <ogupta4242@gmail.com>
2026-05-27 11:31:00 -0700

    refactor(home): apply ktlint formatting

--- shared/src/commonMain/kotlin/xyz/malefic/icecreammint/screens/HomeScreen.kt
+++ shared/src/commonMain/kotlin/xyz/malefic/icecreammint/screens/HomeScreen.kt
@@ -3,20 +3,13 @@ package xyz.malefic.icecreammint.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun HomeScreen() {
     Column(
@@ -27,8 +20,5 @@ fun HomeScreen() {
     {
         Text(" ")
         Text("Welcome to Ice Cream Mint!", fontSize = 30.sp, fontFamily = FontFamily.Serif)
-
     }
 }
-
-
